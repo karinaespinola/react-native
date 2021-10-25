e2fbaa7a2f3e8f6e988118db684b4b4e256303c4
Karina Espinola <k.espinolah@gmail.com>
2021-10-25 15:28:04 -0600

    Add method to update the colors with a validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,20 +2,52 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [blue, setBlue] = useState(0);
     const [green, setGreen] = useState(0);
 
+    setColor = (color, change) => {
+
+        switch(color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change);
+            return;
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+            return;
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+            return;
+            default:
+            return;
+        }
+    }
+
     return <View>
         <ColorCounter 
-            onIncrease={() => setRed(red + 1)}
-            onDecrease={() => setRed(red - 1)} 
+            onIncrease={() => setColor('red', COLOR_INCREMENT)}
+            onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)} 
             color="Red"
         />
-        <ColorCounter color="Blue"/>
-        <ColorCounter color="Green"/>
+        <ColorCounter             
+            onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+            onDecrease={() => setGreen(green - COLOR_INCREMENT)} 
+            color="Green"
+        />
+        <ColorCounter             
+            onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+            onDecrease={() => setBlue(blue - COLOR_INCREMENT)} 
+            color="Blue"
+        />
+        <View style={{height: 150, width: 150, backgroundColor:`rgb(${red}, ${green}, ${blue})`}} />
+        <View>
+            <Text>
+                Current color: {`rgb(${red}, ${green}, ${blue})`}
+            </Text>
+        </View>
     </View>
 }
 
