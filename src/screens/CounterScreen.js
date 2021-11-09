a74e58ca9a0016281c54b9522e4b09cef02f6325
Karina Espinola <k.espinolah@gmail.com>
2021-11-09 15:16:51 -0600

    Change the CounterScreen component to use a reducer to manage the state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,24 +1,35 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const COUNT_INCREMENT = 1;
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'increase':
+            return { ...state, counter: state.counter + COUNT_INCREMENT };
+        case 'decrease':
+            return { ...state, counter: state.counter - COUNT_INCREMENT };
+    }
+};
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
 
     return <View>
         <View>
             <Button 
                 title="Increase"
                 onPress={() => {
-                   setCounter(counter + 1);
+                   dispatch({ type: 'increase', payload: COUNT_INCREMENT });
                 }}
             />
             <Button 
                 title="Decrease"
                 onPress={() => {
-                    setCounter(counter -1);
+                    dispatch({ type: 'decrease', payload: COUNT_INCREMENT });
                 }}
             />
-           <Text>Current Count: {counter}</Text>
+           <Text>Current Count: {state.counter}</Text>
         </View>
     </View>
 };
