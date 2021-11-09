045137b61d7ad38bbb2a7c6e5e885554e56216ec
Karina Espinola <k.espinolah@gmail.com>
2021-11-08 18:09:20 -0600

    Use reducer to handle the SquareScreen state

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -13,30 +13,30 @@ const reducer = (state, action) => {
             // never going to do: state.red = state.red - 15;
             return { ...state, red: state.red + action.amount};
         case 'green':
-            return { ...state, red: state.green + action.amount};
+            return { ...state, green: state.green + action.amount};
         case 'blue':
-            return { ...state, red: state.red + action.amount};
+            return { ...state, blue: state.blue + action.amount};
         default:
     }
 };
 
 const SquareScreen = () => {
     const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
-
+    const { red, green, blue } = state;
     return <View>
         <ColorCounter 
-            onIncrease={() => }
-            onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)} 
+            onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })} 
             color="Red"
         />
         <ColorCounter             
-            onIncrease={() => }
-            onDecrease={() => } 
+            onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })} 
             color="Green"
         />
         <ColorCounter             
-            onIncrease={() => }
-            onDecrease={() => } 
+            onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })} 
             color="Blue"
         />
         <View style={{height: 150, width: 150, backgroundColor:`rgb(${red}, ${green}, ${blue})`}} />
