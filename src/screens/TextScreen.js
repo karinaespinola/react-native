bf8d7b9c01deaf0328c497df495722b83a49cf9e
Karina Espinola <k.espinolah@gmail.com>
2021-11-09 15:48:00 -0600

    Add TextScreen component to the Home component

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const TextScreen = () => {
+    return <Text>This is the TextScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TextScreen;
