ae6ba5c5e88c4491305b7c525887bfd090f7f8d5
Karina Espinola <k.espinolah@gmail.com>
2021-11-22 17:38:38 -0600

    Add text input, styles and properties to the TextScreen component

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
-    return <Text>This is the TextScreen</Text>;
+    return <View>
+        <TextInput style={styles.input} />
+    </View>    
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth:1
+  }
+});
 
 export default TextScreen;
