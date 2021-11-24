0c92f4475bac7460205b132f462e4c4449a2bb79
Karina Espinola <k.espinolah@gmail.com>
2021-11-23 18:25:27 -0600

    alignitems property

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Box Screen</Text>
+        <Text style={styles.textStyle}>Child #1</Text>
+        <Text style={styles.textStyle}>Child #2</Text>
+        <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 };
@@ -12,12 +14,14 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 1,
-    borderColor: 'black'
+    borderColor: 'black',
+    flexDirection: 'row',
+    height: 200,
+    alignItems: 'center'
   },
   textStyle: {
-    borderWidth: 10,
+    borderWidth: 1,
     borderColor: 'red',
-    margin:20
   }
 });
 
